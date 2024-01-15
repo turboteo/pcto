813e9f3f8cb51bfb7b18a9ab49c015cce2f5973e
turboteo <156682859+turboteo@users.noreply.github.com>
2024-01-15 15:19:11 +0100

    Add files via upload

---
+++ ex01.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+
+
+
+int main(){ 
+
+    printf("Ciao mi chiamo Rat e sono un caga sbubub \n");
+    return 0;
+}
---
+++ ex02.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main(){ 
+
+    int x = 1;
+    float y = 9.99999;
+    char z = 'A';
+    double u = 4.853;
+
+    printf("%d \n %f \n %c \n %lf \n", x, y, z, u);
+
+    return 0;
+}
