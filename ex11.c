098aa42deb63f765398d1dd21e9972df2284a7e8
turboteo <156682859+turboteo@users.noreply.github.com>
2024-01-16 16:54:08 +0100

    Add files via upload

---
+++ ex11.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+
+int main(){
+
+    int a;
+    int b;
+    int c;
+
+   printf("digita il primo numero: \n");
+   scanf("%d", &a);
+
+   printf("digita il secondo numero: \n");
+   scanf("%d", &b);
+
+   printf("digita il terzo numero: \n");
+   scanf("%d", &c);
+
+   if (a == b && b == c){
+    printf("Il triangolo è equilatero \n");
+   }
+   else if((a == b && b != c) || (a == c && b !=c) || (b == c && c != a)){
+    printf("Il triangolo è isocele \n");
+   }
+   else{}
+
+    return 0;
+}
