0ccb25bdc9417eaf5cd00a118aa735a2ef53e876
turboteo <156682859+turboteo@users.noreply.github.com>
2024-01-16 14:29:59 +0100

    Add files via upload

---
+++ ex08.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+
+int main(){
+
+    int anno;
+    
+
+    printf("Digita un anno: ");
+    scanf("%d", &anno);
+
+    if (anno % 4 == 0){
+        if(anno % 100 != 0){
+            printf("L'anno è bisestile \n");
+        } //negazione della condizione
+
+        else if(anno % 400 == 0){ // passo alla seconda verifica nel caso la prima condizione sia parzialmente corretta
+            printf("L'anno è bisestile \n");
+        }
+
+        else{
+            printf("L'anno non è bisestile \n");
+        }
+    }
+    else{
+        printf("L'anno non è bisestile \n");
+    }
+
+}
