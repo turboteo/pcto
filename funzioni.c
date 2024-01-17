e1821f1081cd7996dc388e1d9462aa25e1cf1681
turboteo <156682859+turboteo@users.noreply.github.com>
2024-01-17 11:15:21 +0100

    Add files via upload

---
+++ funzioni.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+void calcolaCubo() {
+    int numero;
+    int cubo;
+
+    printf("Inserisci un numero: ");
+    scanf("%d", &numero);
+    cubo = numero * numero * numero;
+    printf("Il cubo di %d è %d \n", numero, cubo);
+}
+
+int main() {
+    calcolaCubo();
+    calcolaCubo();
+    calcolaCubo();
+    calcolaCubo();
+    return 0;
+}
