e075cd934bde0e82e4475b01c3658b7db5c38b90
turboteo <156682859+turboteo@users.noreply.github.com>
2024-01-17 15:53:09 +0100

    Add files via upload

--- ex15.c
+++ ex15.c
@@ -1,10 +1,10 @@
 #include <stdio.h>
 
 int main(){
-    int i = 102;
+    int i;
 
-    while(i >= 1){
-        i= i-2;
+    for(i = 101 ;i >= 1; i--){
+        i = i - 1;
         printf("%d \n", i);
     }
 }
